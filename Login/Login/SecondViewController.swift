c76c53c9b58b19ce46d6f57ea61be5a60fb4faf5
eugenegodzishevsky <109996712+eugenegodzishevsky@users.noreply.github.com>
2023-07-22 00:40:43 +1000

    Add files via upload

---
+++ Login/Login/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Vermut xxx on 21.07.2023.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Vermut xxx on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    //MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
